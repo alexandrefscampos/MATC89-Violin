233ad4324b3e66d08ba2e1f00d760d3fd0c8a71a
AlexandreCampos <alexandre.campos@reidopitaco.com>
2024-08-20 20:02:55 -0300

    creating startup of home

---
+++ lib/core/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class VColors {
+  static const primary = Color(0xff1F1D36);
+  static const secondary = Color(0xffE9A6A6);
+  static const tertiary = Color(0xff9C4A8B);
+  static const white = Color(0xffffffff);
+}
