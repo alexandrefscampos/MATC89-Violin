27fd3bd109bce3a7932e274bbfea1f7065677604
AlexandreCampos <alexandre.campos@reidopitaco.com>
2024-08-20 20:02:55 -0300

    creating startup of home

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:violin/core/colors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _actualIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: VColors.primary,
+      drawer: const Drawer(
+        backgroundColor: VColors.primary,
+        child: Column(children: []),
+      ),
+      appBar: AppBar(
+        backgroundColor: VColors.primary,
+        elevation: 0,
+        actions: const [CircleAvatar()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _actualIndex,
+        onTap: (value) => setState(() {
+          _actualIndex = value;
+        }),
+        backgroundColor: VColors.primary,
+        selectedItemColor: VColors.secondary,
+        unselectedItemColor: VColors.white.withOpacity(0.5),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: ''),
+        ],
+      ),
+      body: IndexedStack(
+        index: _actualIndex,
+        children: screenList,
+      ),
+    );
+  }
+}
+
+final screenList = [Container(), Container(), Container()];
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:violin/main.dart';
+import 'package:violin/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
