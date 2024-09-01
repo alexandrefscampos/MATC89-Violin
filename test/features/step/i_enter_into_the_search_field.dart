e7d3a340031b19ff83e08cfb4cb9188dd3ef7bbf
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2024-09-01 19:53:35 -0300

    remove unused file

--- test/features/step/i_enter_into_the_search_field.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-/// Usage: I enter {'Thriller'} into the search field
-Future<void> iEnterIntoTheSearchField(
-    WidgetTester tester, String param1) async {
-  throw UnimplementedError();
-}
